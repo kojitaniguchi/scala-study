b9a7b84a0097fb09238d44a49330343dae4a7c57
kojitaniguchi <tanigu0511@gmail.com>
2017-11-17 17:39:36 +0900

    WarshallFloyd

--- ShortestPath.scala
+++ ShortestPath.scala
@@ -70,4 +70,16 @@ object ShortestPath {
   println(distances(goal))
 }
 
+def solveByWarshallFloyd(start: Char, goal: Char): Unit = {
+  // 二頂点間の距離の初期化
+  var distanceMap: Map[(Char, Char), Int] = vertexes.map(v => ((v , v) -> 0)).toMap
+  distanceMap = distanceMap ++ edges.map(e => (e.from, e.to) -> e.distance)
+  def distance(v1: Char, v2: Char): Int = distanceMap.getOrElse((v1, v2), Int.MaxValue / 2)
+  for (v1 <- vertexes; v2 <- vertexes; v3 <- vertexes) {
+    distanceMap =  distanceMap + ((v2, v3) -> math.min(distance(v2, v3), distance(v2, v1) + distance(v1, v3)))
+  }
+  println(distanceMap)
+  println(distanceMap((start, goal)))
+}
+
 }
